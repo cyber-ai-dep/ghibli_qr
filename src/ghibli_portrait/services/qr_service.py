fdfc0f2f13ca2b2a11eaa897a8f6e0836ffbcc60
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2025-12-23 16:56:16 +0300

    ✨ (qr_service): Return QR image instead of saving to file.

--- src/ghibli_portrait/services/qr_service.py
+++ src/ghibli_portrait/services/qr_service.py
@@ -1,30 +1,29 @@
-from src.ghibli_portrait.config import Settings
-from PIL import Image
 import qrcode
+from PIL import Image
 
-def get_qr(url: str, version: int = None):
+from src.ghibli_portrait.config import Settings
+
+
+def get_qr(url: str, version: int = None) -> Image:
     s = Settings()
 
-    lock_img = Image.open(s.LOCK_PATH).convert('RGBA')
+    lock_img = Image.open(s.LOCK_PATH).convert("RGBA")
 
     qr = qrcode.QRCode(
         version or s.QR_VERSION,
         error_correction=qrcode.ERROR_CORRECT_M,
         box_size=10,
-        border=0
+        border=0,
     )
 
     qr.add_data(url)
 
     qr.make(fit=True)
-    qr_img = qr.make_image(fill_color=s.QR_FILL_COLOR, back_color=s.QR_BACK_COLOR).convert('RGBA')
+    qr_img = qr.make_image(
+        fill_color=s.QR_FILL_COLOR, back_color=s.QR_BACK_COLOR
+    ).convert("RGBA")
 
     qr_img = qr_img.resize((s.QR_SIDE_LEN, s.QR_SIDE_LEN), Image.LANCZOS)
     lock_img.paste(qr_img, s.QR_PASTE_COORDINATES, qr_img)
 
-    lock_img.save('google_qr.png')
-
-
-
-if __name__ == '__main__':
-    get_qr('http://google.com')
+    return lock_img
