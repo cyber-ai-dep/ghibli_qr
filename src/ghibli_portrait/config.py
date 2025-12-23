7294c486e84a416ac2616c8d01e5d22a40c1c645
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2025-12-23 16:08:15 +0300

    feat(config): add QR config and lock path

--- src/ghibli_portrait/config.py
+++ src/ghibli_portrait/config.py
@@ -1,1 +1,16 @@
-# settings
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+
+class Setting:
+    # QR Settings
+
+    QR_VERSION = 1
+    FILL_COLOR = ("white",)
+    BACK_COLOR = "#2a2d42"
+    PASTE_COORDINATES = (817, 795)
+
+    LOCK_PATH = Path(__file__).parent.parent / "static" / "lock.png"
