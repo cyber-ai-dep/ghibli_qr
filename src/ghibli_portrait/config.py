c1cbeb90b8026361c26b3f585551157c0fc3cbcf
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2025-12-23 16:54:16 +0300

    ✨ (qr_service): Add QR generation and embed into lock image

--- src/ghibli_portrait/config.py
+++ src/ghibli_portrait/config.py
@@ -5,12 +5,13 @@ from pathlib import Path
 load_dotenv()
 
 
-class Setting:
+class Settings:
     # QR Settings
 
     QR_VERSION = 1
-    FILL_COLOR = ("white",)
-    BACK_COLOR = "#2a2d42"
-    PASTE_COORDINATES = (817, 795)
+    QR_FILL_COLOR = "white"
+    QR_BACK_COLOR = "#2a2d42"
+    QR_PASTE_COORDINATES = (817, 795)
+    QR_SIDE_LEN = 685
 
     LOCK_PATH = Path(__file__).parent.parent / "static" / "lock.png"
