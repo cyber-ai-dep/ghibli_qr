72371e221976aff9a0e6d9a2110a69574e29beea
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2025-12-24 10:03:39 +0300

    ✨ (image_service): Add get_ghibli to call KIE API for Ghibli style art

--- src/ghibli_portrait/services/image_service.py
+++ src/ghibli_portrait/services/image_service.py
@@ -1,1 +1,38 @@
-# ghibli transformation + ghibli and qr merge
+from typing import List
+
+import requests
+
+from src.ghibli_portrait.config import Settings
+from src.ghibli_portrait.models.schemas import AspectRatio, Quality
+
+imgURLs = List[str]
+
+
+def get_ghibli(
+    imgs: imgURLs,
+    aspect_ratio: AspectRatio = AspectRatio._1_1,
+    quality: Quality = Quality.BASIC,
+) -> dict:
+    s = Settings()
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {s.KIE_API_KEY}",
+    }
+
+    body = {
+        "model": s.KIE_IMG_MODEL,
+        "callBackUrl": s.CALL_BACK,
+        "input": {
+            "prompt": "Convert this image to Ghibli style art.",
+            "image_urls": imgs,
+            "aspect_ratio": aspect_ratio,
+            "quality": quality,
+        },
+    }
+
+    try:
+        response = requests.post(s.KIE_CREATE_TASK_API, json=body, headers=header)
+        return response.json()
+
+    except Exception as e:
+        raise
