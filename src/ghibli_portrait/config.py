552c65ed5f51bc640199402169670fd184ac8379
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2025-12-24 10:04:31 +0300

    ✨ (config): Add KIE API and server settings to Settings class

--- src/ghibli_portrait/config.py
+++ src/ghibli_portrait/config.py
@@ -1,13 +1,13 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Settings:
     # QR Settings
-
     QR_VERSION = 1
     QR_FILL_COLOR = "white"
     QR_BACK_COLOR = "#2a2d42"
@@ -15,3 +15,12 @@ class Settings:
     QR_SIDE_LEN = 685
 
     LOCK_PATH = Path(__file__).parent.parent / "static" / "lock.png"
+
+    # Kie Settings
+    KIE_API_KEY = os.getenv["KIE_API_KEY"]
+    KIE_IMG_MODEL = os.getenv["KIE_IMG_MODEL"]
+    KIE_CREATE_TASK_API = 'https://api.kie.ai/api/v1/jobs/createTask'
+
+    # Server Settings
+    DOMAIN = os.getenv("DOMAIN")
+    CALL_BACK = DOMAIN + '/api/callback'
